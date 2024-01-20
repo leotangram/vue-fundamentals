39593333ba98af4a1e59a6f9eb89df7eccf92ee5
Leonardo Omaña <leotangram@gmail.com>
2024-01-19 22:20:07 -0500

    define props

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <Counter />
+  <Counter :start="10" />
 </template>
 
 <script>
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,31 +1,63 @@
 <template>
-  <h2>Counter</h2>
+  <h2>{{ customTitle }} {{ start }}</h2>
   <p>{{ counter }}<sup>2</sup> = {{ squareCounter }}</p>
-  <p>{{ counter }}<sup>2</sup> = {{ squareCounter }}</p>
-  <p>{{ counter }}<sup>2</sup> = {{ squareCounter }}</p>
-  <p>{{ counter }}<sup>2</sup> = {{ squareCounter }}</p>
+  <div>
+    <button @click="decrement">-1</button>
+    <button @click="increment">+1</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Counter",
+  props: {
+    title: {
+      type: String,
+      required: false,
+    },
+    start: {
+      type: Number,
+      default: 100,
+    },
+  },
   data() {
     return {
-      counter: 5,
+      counter: this.start,
     };
   },
   methods: {
-    getSquareValue() {
-      return this.counter * this.counter;
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
     },
   },
   computed: {
     squareCounter() {
       return this.counter * this.counter;
     },
+    customTitle() {
+      return this.title || "Counter";
+    },
   },
 };
 </script>
 
 <style>
+button {
+  background-color: #64bb87;
+  border-radius: 5px;
+  border: 1px solid white;
+  color: white;
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+}
+
+button:hover {
+  background-color: #5aa67b;
+  transition: 0.3s ease-in-out;
+}
 </style>
